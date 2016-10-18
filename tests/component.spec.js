f0890115e389dbaec924bc3b82dc23f763b45fe7
Yiyi Sun <yiyisun@gmail.com>
2016-10-18 13:22:50 -0400

    Update unit tests for Component

--- tests/component.spec.js
+++ tests/component.spec.js
@@ -40,14 +40,212 @@
 /******/ 	return __webpack_require__(0);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
-/***/ function(module, exports) {
+/******/ ({
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
+	var index_1 = __webpack_require__(103);
+	var component_1 = __webpack_require__(104);
+	var model = 'x';
+	var view = function (_) { return ''; };
+	var update = {
+	    hi: function (_, val) { return val; }
+	};
 	describe('Component', function () {
+	    it('should trigger update', function () {
+	        spyOn(update, 'hi');
+	        var component = new component_1.default(document.body, model, view, update);
+	        index_1.default.run('hi', 'xx');
+	        expect(update.hi).toHaveBeenCalledWith('x', 'xx');
+	    });
+	    it('should trigger view', function () {
+	        var view = jasmine.createSpy('view');
+	        var component = new component_1.default(document.body, model, view, update);
+	        expect(view).toHaveBeenCalledWith('x');
+	        index_1.default.run('hi', 'xx');
+	        expect(view).toHaveBeenCalledWith('xx');
+	    });
+	    it('should track history', function () {
+	        var view = jasmine.createSpy('view');
+	        var component = new component_1.default(document.body, model, view, update, { history: true });
+	        index_1.default.run('hi', 'xx');
+	        index_1.default.run('hi', 'xxx');
+	        index_1.default.run('history-prev');
+	        index_1.default.run('history-next');
+	        expect(view.calls.allArgs()).toEqual([['x'], ['xx'], ['xxx'], ['xx'], ['xxx']]);
+	    });
+	    it('should track history with custom event name', function () {
+	        var view = jasmine.createSpy('view');
+	        var component = new component_1.default(document.body, model, view, update, { history: { prev: 'prev', next: 'next' } });
+	        index_1.default.run('hi', 'xx');
+	        index_1.default.run('hi', 'xxx');
+	        index_1.default.run('prev');
+	        index_1.default.run('next');
+	        expect(view.calls.allArgs()).toEqual([['x'], ['xx'], ['xxx'], ['xx'], ['xxx']]);
+	    });
 	});
 
 
+/***/ },
+
+/***/ 1:
+/***/ function(module, exports) {
+
+	"use strict";
+	var App = (function () {
+	    function App() {
+	        this._events = {};
+	    }
+	    App.prototype.on = function (name, fn, options) {
+	        if (options === void 0) { options = {}; }
+	        if (options.debug)
+	            console.debug('on: ' + name);
+	        this._events[name] = this._events[name] || [];
+	        this._events[name].push({ fn: fn, options: options });
+	    };
+	    App.prototype.run = function (name) {
+	        var _this = this;
+	        var args = [];
+	        for (var _i = 1; _i < arguments.length; _i++) {
+	            args[_i - 1] = arguments[_i];
+	        }
+	        var subscribers = this._events[name];
+	        console.assert(!!subscribers, 'No subscriber for event: ' + name);
+	        if (subscribers)
+	            this._events[name] = subscribers.filter(function (sub) {
+	                var fn = sub.fn, options = sub.options;
+	                if (options.delay) {
+	                    _this.delay(name, fn, args, options);
+	                }
+	                else {
+	                    if (options.debug)
+	                        console.debug('run: ' + name, args);
+	                    fn.apply(_this, args);
+	                }
+	                return !sub.options.once;
+	            });
+	    };
+	    App.prototype.delay = function (name, fn, args, options) {
+	        var _this = this;
+	        if (options._t)
+	            clearTimeout(options._t);
+	        options._t = setTimeout(function () {
+	            clearTimeout(options._t);
+	            if (options.debug)
+	                console.debug(("run-delay " + options.delay + ":") + name, args);
+	            fn.apply(_this, args);
+	        }, options.delay);
+	    };
+	    return App;
+	}());
+	exports.App = App;
+	var app = new App();
+	Object.defineProperty(exports, "__esModule", { value: true });
+	exports.default = app;
+
+
+/***/ },
+
+/***/ 103:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var app_1 = __webpack_require__(1);
+	var component_1 = __webpack_require__(104);
+	Object.defineProperty(exports, "__esModule", { value: true });
+	exports.default = app_1.default;
+	app_1.default.start = function (element, model, view, update, options) {
+	    return new component_1.default(element, model, view, update, options);
+	};
+	if (typeof window === 'object') {
+	    window['app'] = app_1.default;
+	}
+
+
+/***/ },
+
+/***/ 104:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var app_1 = __webpack_require__(1);
+	var Component = (function () {
+	    function Component(element, state, view, update, options) {
+	        var _this = this;
+	        if (update === void 0) { update = {}; }
+	        this.element = element;
+	        this.state = state;
+	        this.view = view;
+	        this._history = [];
+	        this._history_idx = -1;
+	        console.assert(!!element);
+	        options = options || {};
+	        this.enable_history = !!options.history;
+	        if (this.enable_history) {
+	            app_1.default.on(options.history.prev || 'history-prev', function () {
+	                _this._history_idx--;
+	                if (_this._history_idx >= 0) {
+	                    _this.set_state(_this._history[_this._history_idx]);
+	                }
+	                else {
+	                    _this._history_idx = 0;
+	                }
+	            });
+	            app_1.default.on(options.history.next || 'history-next', function () {
+	                _this._history_idx++;
+	                if (_this._history_idx < _this._history.length) {
+	                    _this.set_state(_this._history[_this._history_idx]);
+	                }
+	                else {
+	                    _this._history_idx = _this._history.length - 1;
+	                }
+	            });
+	        }
+	        this.view = view;
+	        this.add_actions(update);
+	        this.push_state(state);
+	    }
+	    Object.defineProperty(Component.prototype, "State", {
+	        get: function () {
+	            return this.state;
+	        },
+	        enumerable: true,
+	        configurable: true
+	    });
+	    Component.prototype.set_state = function (state) {
+	        this.state = state;
+	        if (this.view) {
+	            this.view(this.state);
+	        }
+	    };
+	    Component.prototype.push_state = function (state) {
+	        this.set_state(state);
+	        if (this.enable_history) {
+	            this._history = this._history.concat([state]);
+	            this._history_idx = this._history.length - 1;
+	        }
+	    };
+	    Component.prototype.add_actions = function (actions) {
+	        var _this = this;
+	        Object.keys(actions).forEach(function (action) {
+	            app_1.default.on(action, function () {
+	                var p = [];
+	                for (var _i = 0; _i < arguments.length; _i++) {
+	                    p[_i - 0] = arguments[_i];
+	                }
+	                _this.push_state(actions[action].apply(actions, [_this.State].concat(p)));
+	            });
+	        });
+	    };
+	    return Component;
+	}());
+	Object.defineProperty(exports, "__esModule", { value: true });
+	exports.default = Component;
+	;
+
+
 /***/ }
-/******/ ]);
+
+/******/ });
--- tests/component.spec.ts
+++ tests/component.spec.ts
@@ -1,7 +1,47 @@
-import {} from 'jasmine';
-import Component from '../component';
+import { } from 'jasmine';
+import app from '../apprun-zero/index';
+import Component from '../apprun-zero/component';
+
+const model = 'x';
+const view = _ => '';
+const update = {
+  hi: (_, val) => val
+}
 
 describe('Component', () => {
+  it('should trigger update', () => {
+    spyOn(update, 'hi');
+    const component = new Component(document.body, model, view, update);
+    app.run('hi', 'xx');
+    expect(update.hi).toHaveBeenCalledWith('x', 'xx');
+  });
 
+  it('should trigger view', () => {
+    const view = jasmine.createSpy('view');
+    const component = new Component(document.body, model, view, update);
+    expect(view).toHaveBeenCalledWith('x');
+    app.run('hi', 'xx');
+    expect(view).toHaveBeenCalledWith('xx');
+  });
+
+  it('should track history', () => {
+    const view = jasmine.createSpy('view');
+    const component = new Component(document.body, model, view, update, {history:true});
+    app.run('hi', 'xx');
+    app.run('hi', 'xxx');
+    app.run('history-prev');
+    app.run('history-next');
+    expect(view.calls.allArgs()).toEqual([['x'], ['xx'], ['xxx'], ['xx'], ['xxx']]);
+  });
+
+  it('should track history with custom event name', () => {
+    const view = jasmine.createSpy('view');
+    const component = new Component(document.body, model, view, update, {history:{prev:'prev', next:'next'}});
+    app.run('hi', 'xx');
+    app.run('hi', 'xxx');
+    app.run('prev');
+    app.run('next');
+    expect(view.calls.allArgs()).toEqual([['x'], ['xx'], ['xxx'], ['xx'], ['xxx']]);
+  });
 
 })
