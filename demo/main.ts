bdc49bdb82fa7cbfce999b87e03dde0903819652
Eric Sun <eric.sun@brookfieldrps.com>
2017-02-07 15:53:44 -0500

    Add demo of drag and drop demo

--- demo/main.ts
+++ demo/main.ts
@@ -3,9 +3,10 @@ import home from './router-components/home';
 import counter from './router-components/counter';
 import counters from './router-components/counters';
 import echo from './router-components/echo';
+import dragdrop from './router-components/dragdrop';
 
 const element = document.getElementById('my-app');
-[home, counter, counters, echo].forEach(c => c(element));
+[home, counter, counters, echo, dragdrop].forEach(c => c(element));
 
 app.run('/');
 
---
+++ demo/router-components/dragdrop.tsx
@@ -0,0 +1,52 @@
+import app from '../../index-jsx';
+
+const model = {
+  dragging: false,
+  position: { x: 100, y: 100 },
+  start: { x: 0, y: 0 },
+}
+
+const view = (model) => {
+
+  const style = {
+    userSelect: 'none',
+    cursor: 'move',
+    position: 'absolute',
+    padding: '50px',
+    border: '1px solid black',
+    color: model.dragging ? 'gold' : 'white',
+    "background-color": '#3C8D2F',
+    left: `${model.position.x}px`,
+    top:  `${model.position.y}px`
+  };
+
+  return <div
+    onmousedown = { e => app.run('drag', e)}
+    onmousemove = { e => app.run('move', e)}
+    onmouseup =   { e => app.run('drop')}
+    style={style}
+    > Drag me!
+  </div>
+}
+
+const update = {
+  '#dragdrop': (model) => model,
+  drag: (model, e) => ({ ...model,
+    dragging: true,
+    start: {x: e.pageX, y: e.pageY}
+  }),
+  move: (model, e) => {
+    if (!model.dragging) return { ...model,
+      view: ()=>{} // this is tells app not to call view function
+    };
+    const start = {x: e.pageX, y: e.pageY}
+    const position = {
+      x: model.position.x + e.pageX - model.start.x,
+      y: model.position.y + e.pageY - model.start.y
+    }
+    return ({...model, start, position})
+  },
+  drop: (model) => ({ ...model, dragging: false })
+}
+
+export default (element) => app.start(element, model, view, update);
