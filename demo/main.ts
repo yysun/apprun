79bec0ddcfcf5862624d841b4aadab8db5ce24bc
Eric Sun <eric.sun@brookfieldrps.com>
2017-02-07 17:17:09 -0500

    Add demo of todo list

--- demo/main.ts
+++ demo/main.ts
@@ -4,9 +4,10 @@ import counter from './router-components/counter';
 import counters from './router-components/counters';
 import echo from './router-components/echo';
 import dragdrop from './router-components/dragdrop';
+import todo from './router-components/todo';
 
 const element = document.getElementById('my-app');
-[home, counter, counters, echo, dragdrop].forEach(c => c(element));
+[home, counter, counters, echo, dragdrop, todo].forEach(c => c(element));
 
 app.run('/');
 
---
+++ demo/router-components/todo.tsx
@@ -0,0 +1,82 @@
+import app from '../../index-jsx';
+
+enum Filters { all = 0, todo = 1, done = 2 }
+
+declare interface ITodo {
+  value: string,
+  done: boolean
+}
+
+declare interface IModel {
+  filter: Filters,
+  todos: Array<ITodo>;
+}
+
+const model = {
+  filter: 0,
+  todos: [],
+  input: ''
+};
+
+const Todo = ({todo, idx}) => (<li onclick={()=>app.run('toggle', idx)}
+  style = {{
+    color: todo.done ? 'green': 'red',
+    textDecoration: todo.done ? "line-through" : "none",
+    cursor: 'pointer'
+  }}>
+  {todo.value}
+</li>);
+
+const view = (model) => {
+  const styles = (filter) => ({
+    'font-weight': model.filter === filter ? 'bold' : 'normal',
+    cursor: 'pointer'
+  })
+  return <div>
+    <h1>Todo</h1>
+    <span>Show:</span>
+    <span> <a style={styles(0)} onclick={()=>app.run('filter', 0)}>All</a></span> |
+    <span> <a style={styles(1)} onclick={()=>app.run('filter', 1)}>Todo</a></span> |
+    <span> <a style={styles(2)} onclick={()=>app.run('filter', 2)}>Done</a></span>
+    <ul>
+      {
+        model.todos
+          .filter(todo => model.filter === 0 ||
+            (model.filter === 1 && !todo.done) ||
+            (model.filter === 2 && todo.done) )
+          .map((todo, idx) => <Todo todo={todo} idx={idx} />)
+      }
+    </ul>
+    <div>
+      <input id='new-todo' placeholder='add todo' onkeyup={e => { if (e.keyCode === 13) add()} }/>
+      <button onclick={()=>add()}>Add</button>
+      <button onclick={()=>app.run('clear')}>Clear</button>
+    </div>
+  </div>
+}
+
+const add = () => {
+  const input = (document.getElementById('new-todo') as HTMLInputElement);
+  if (input.value) {
+    app.run('add', input.value);
+    input.value = '';
+  }
+};
+
+const update = {
+  '#todo': model => model,
+  add: (model, value) => ({...model,
+    todos: [...model.todos, {value, done:false}]
+  }),
+  toggle: (model, idx) => ({...model,
+    todos: [
+      ...model.todos.slice(0, idx),
+      {...model.todos[idx], done: !model.todos[idx].done},
+      ...model.todos.slice(idx + 1)
+    ]
+  }),
+  filter: (model, filter) => ({...model, filter}),
+  clear: (model) => ({...model, todos:[] })
+}
+
+export default (element) => app.start(element, model, view, update);
