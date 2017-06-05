98395be2fbdc0cd191841299932f34f1b67559f2
Yiyi Sun <yiyisun@gmail.com>
2017-06-05 13:56:22 -0400

    remove virtual-dom

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
 import app, { Model, View, Update } from './index-zero';
 import Router from './router';
-import { updateElement } from './vdom';
+import { h, render } from './vdom';
 import ComponentBase from './component';
 
 export { Router };
 export class Component extends ComponentBase {
   protected initVdom() {
-    this.updateElement =  updateElement.bind(this);
+    this.updateElement =  render.bind(this);
   }
 }
 
 app.start = (element: HTMLElement, model: Model, view: View, update: Update, options?) =>
   new Component(element, model, view, update, options);
+
+app.createElement = app.h = h;
 
 export default app;
--- vdom.ts
+++ vdom.ts
@@ -1,18 +1,11 @@
 /// <reference path="./virtual-dom.d.ts" />
 
-import h = require('virtual-dom/h');
-import patch = require('virtual-dom/patch');
-import diff = require('virtual-dom/diff');
-import VNode = require('virtual-dom/vnode/vnode');
-import VText = require('virtual-dom/vnode/vtext');
-import createElement = require('virtual-dom/create-element');
-import virtualize = require('vdom-virtualize');
-
+import { h, updateElement } from './vdom-my';
 import morph = require('morphdom')
 
-export function updateElement(element, html) {
+export { h };
+export function render(element, html) {
   console.assert(!!element);
-
   if (typeof html === 'string') {
     if (element.firstChild) {
       morph(element.firstChild, html);
@@ -20,23 +13,8 @@ export function updateElement(element, html) {
       element.innerHTML = html;
     }
   } else {
-    const vtree = html;
-    if (element.firstChild) {
-      const prev = element.firstChild.vtree || virtualize(element.firstChild);
-      const patches = diff(prev, vtree);
-      patch(element.firstChild, patches);
-    } else {
-      const node = createElement(vtree);
-      element.appendChild(node);
-    }
-    element.firstChild.vtree = vtree;
+    render(element, html);
   }
 }
-
-import app from './app';
-app.h = (el, props, ...children) => (typeof el === 'string') ?
-    h(el, props, children) : el(props, children);
-
-app.createElement = app.h;
 
 
