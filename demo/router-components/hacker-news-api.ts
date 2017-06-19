caf6ab9094acb6aedf8d6f3e1db03a02820f461c
Yiyi Sun <yiyisun@gmail.com>
2017-06-19 13:04:39 -0400

    Demo: HN: add best type, Re-org api

--- demo/router-components/hacker-news-api.ts
+++ demo/router-components/hacker-news-api.ts
@@ -3,9 +3,43 @@ import Firebase = require('firebase');
 Firebase.initializeApp({ databaseURL: 'https://hacker-news.firebaseio.com'});
 const db = Firebase.database().ref('/v0');
 
-export default async function (path): Promise<any> {
+export async function fetch(path): Promise<any> {
   const ref = db.child(path);
   return new Promise((resolve, reject) => {
     ref.once('value', snapshot => resolve(snapshot.val()), reject);
   })
+}
+
+export async function fetchList(type): Promise<any> {
+  return fetch(`${type}stories`);
+}
+
+
+type Item = any & {
+  type: 'story' | 'comment' | 'job' | 'poll',
+  items: List
+} //any?
+
+type ListType = 'top' | 'new' | 'best' | 'show' | 'ask' | 'job' | 'comment';
+
+type List = {
+  pageno: number,
+  pages: number,
+  items: Array<Item>
+}
+
+type State = any & { type: ListType } //any?
+
+const page_size = 10;
+
+export async function fetchListItems(list, pageno): Promise<any> {
+  list.pageno = pageno;
+  list.pages = Math.ceil(list.items.length / page_size);
+  if (isNaN(list.pageno) || list.pageno < 1) list.pageno = 1;
+  if (list.pageno > list.pages) list.pageno = list.pages;
+  list.items = await Promise.all(list.items.map(async (item, idx) => {
+    return ((Math.floor(idx / page_size) === list.pageno - 1) && (typeof item === 'number')) ?
+      await fetch(`item/${item}`) : item
+  }));
+  // return list;
 }
--- demo/router-components/hacker-news.tsx
+++ demo/router-components/hacker-news.tsx
@@ -1,19 +1,13 @@
 import app, { Component } from '../../index';
-import fetch from './hacker-news-api';
-
-type ListType = 'top' | 'new' | 'show' | 'ask' | 'job'
-
-type State = {
-  title: ListType
-}
+import { fetch, fetchList, fetchListItems } from './hacker-news-api';
 
 const root = '#hacker-news';
 const page_size = 10;
 
 export class HackerNewsComponent extends Component {
 
-  state: State = {
-    title: 'top'
+  state = {
+    type: 'top'
   };
 
   Item = ({ item, idx }) => {
@@ -38,71 +32,61 @@ export class HackerNewsComponent extends Component {
     </ul>;
   }
 
-  ListHeader = ({ list, title }) => {
+  ListHeader = ({ list, type }) => {
     if (!list) return;
     const style = (enable: boolean) => enable ?
       { cursor: 'pointer' } :
       { 'pointer-events': 'none' };
     return <div style={{ 'margin-left': '50px' }}>
       <span>{list.pageno} / {list.pages} ({list.items.length})</span>
-      &nbsp;&nbsp;<a href={`${root}/${title}/${list.pageno - 1}`} style={style(list.pageno > 1)}>&lt;&lt;</a>
-      &nbsp;&nbsp;<a href={`${root}/${title}/${list.pageno + 1}`} style={style(list.pageno < list.pages)}>&gt;&gt;</a>
+      &nbsp;&nbsp;<a href={`${root}/${type}/${list.pageno - 1}`} style={style(list.pageno > 1)}>&lt;&lt;</a>
+      &nbsp;&nbsp;<a href={`${root}/${type}/${list.pageno + 1}`} style={style(list.pageno < list.pages)}>&gt;&gt;</a>
     </div>
   }
 
   view = (state) => {
     if (state instanceof Promise) return; // ?
-    const style = (title) => {
-      return { 'font-weight': state.title === title ? 'bold' : 'normal' }
+    const style = (type) => {
+      return { 'font-weight': state.type === type ? 'bold' : 'normal' }
     }
-    const list = state[state.title];
+    const list = state[state.type];
     return <div>
       <div style={{ 'display': 'flex', 'align-items': 'baseline' }}>
         <h3 style={{ 'margin-right': '20px' }}>Hacker News</h3>
         <div>
           <a style={style('top')} href={`${root}/top`}>Top</a> |&nbsp;
           <a style={style('new')} href={`${root}/new`}>New</a> |&nbsp;
+          <a style={style('best')} href={`${root}/best`}>Best</a> |&nbsp;
           <a style={style('show')} href={`${root}/show`}>Show</a> |&nbsp;
           <a style={style('ask')} href={`${root}/ask`}>Ask</a> |&nbsp;
           <a style={style('job')} href={`${root}/job`}>Jobs</a>
         </div>
-        <this.ListHeader list={list} title={state.title} />
+        <this.ListHeader list={list} type={state.type} />
       </div>
       <this.List list={list} />
     </div>
   }
 
   update = {
-    '#hacker-news': async (state, title?, pageno?) => {
-      if (!title || !pageno) {
-        title = title || state.title || 'top';
-        pageno = pageno || (state[title] && state[title].pageno) || 1;
-        history.replaceState(null, null, `${root}/${title}/${pageno}`);
+    '#hacker-news': async (state, type?, pageno?) => {
+      if (!type || !pageno) {
+        type = type || state.type || 'top';
+        pageno = pageno || (state[type] && state[type].pageno) || 1;
+        history.replaceState(null, null, `${root}/${type}/${pageno}`);
       }
-      const new_state = { ...this.state, title };
-      if (!new_state[title]) {
-        console.log(`fetch: ${title}`);
-        new_state[title] = {}
-        new_state[title].items = await fetch(`${title}stories`);
+      const new_state = { ...this.state, type };
+      if (!new_state[type]) {
+        console.log(`fetch: ${type}`);
+        new_state[type] = {}
+        new_state[type].items = await fetchList(type);
       }
-      await this.load(new_state[title], parseInt(pageno));
+      await fetchListItems(new_state[type], parseInt(pageno));
       this.setState(new_state); // ?
       // console.log(new_state);
       return new_state;
     }
   }
 
-  load = async (list, pageno) => {
-    list.pageno = pageno;
-    list.pages = Math.ceil(list.items.length / page_size);
-    if (isNaN(list.pageno) || list.pageno < 1) list.pageno = 1;
-    if (list.pageno > list.pages) list.pageno = list.pages;
-
-    list.items = await Promise.all(list.items.map(async (item, idx) => {
-      return ((Math.floor(idx / page_size) === list.pageno - 1) && (typeof item === 'number')) ?
-        await fetch(`item/${item}`) : item
-    }));
-  }
 }
 
 function pluralize(number, label) {
