0aff7734e89bf910cd292165beab984636573a72
Yiyi Sun <yiyisun@gmail.com>
2017-07-04 13:49:16 -0400

    Demo: use generic routing event to update top menu

--- demo/main.ts
+++ demo/main.ts
@@ -1,3 +1,4 @@
+import app from '../index';
 
 import home from './router-components/home';
 import counter from './router-components/counter';
@@ -11,3 +12,10 @@ import hacker_news from './router-components/hacker-news';
 
 const element = document.getElementById('my-app');
 [home, hello, counter, counters, dragdrop, todo, multi, benchmark, hacker_news].forEach(c => c(element));
+
+app.on('//', route => {
+  const menus = document.querySelectorAll('.navbar-nav li');
+  for (let i = 0; i < menus.length; ++i) menus[i].classList.remove('active');
+  const item = document.querySelector(`[href='${route}']`);
+  item && item.parentElement.classList.add('active');
+})
