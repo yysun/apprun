0e8376571273e10f377addc8d7ad6f1eb306f4b5
Yiyi Sun <yiyisun@gmail.com>
2017-09-01 08:42:26 -0400

    Fix typo

--- createComponent.tsx
+++ createComponent.tsx
@@ -6,6 +6,6 @@ export default function (componentClass, id, props) {
   const component = cache[id] ? cache[id] :
     (cache[id] = new componentClass(props).mount(id))
   return <div id={id}>
-    {component.render() && component.render()}
+    {component.render && component.render()}
   </div>
 }
