1ae5bc1336f17f26d4c91188ca0d8376809ead3d
Yiyi Sun <yiyisun@gmail.com>
2017-12-29 17:22:47 -0500

    update decorator to convert method name and property name to action name

--- decorator.ts
+++ decorator.ts
@@ -18,16 +18,18 @@ export const Reflect = {
   }
 }
 
-export function update(name: string, options: any = {}) {
+export function update(name?: string, options: any = {}) {
   return (target: any, key: string, descriptor: any) => {
+    name = `${key}${name ? ',' + name : ''}`;
     Reflect.defineMetadata(`apprun-update:${name}`,
-      { name, action: [descriptor.value, options] }, target)
+      { name, action: [descriptor.value, options] }, target);
     return descriptor;
   }
 }
 
-export function on(name: string, options: any = {}) {
+export function on(name?: string, options: any = {}) {
   return function (target: any, key: string) {
+    name = `${key}${name ? ',' + name : ''}`;    
     Reflect.defineMetadata(`apprun-update:${name}`, { name, key }, target)
   }
 }
--- tests/decorator.spec.tsx
+++ tests/decorator.spec.tsx
@@ -2,30 +2,60 @@ import app, { Component, on, update } from '../index';
 
 describe('Update decorator', () => {
   
-  it('should convert method to action', () => {
+  it('should convert class method to action using @update', () => {
     class TestComponent extends Component {
       view = state => state
 
-      @update('hi, #hi')
+      @update()
       f1(state, val) {
         return val;
       }
 
+      @update('hi, #hi')
+      f2(state, val) {
+        return val;
+      }
+    }
+
+    const test = new TestComponent().mount();
+    const spy = spyOn(test, 'view')
+    test.run('f1', 'x')
+    expect(spy).toHaveBeenCalledWith('x')
+    test.run('f2', 'xx')
+    expect(spy).toHaveBeenCalledWith('xx')
+    // local alias
+    test.run('hi', 'xxx')
+    expect(spy).toHaveBeenCalledWith('xxx')
+    // global alias
+    app.run('#hi', 'xxxx')
+    expect(spy).toHaveBeenCalledWith('xxxx')
+  })
+
+
+  it('should convert class property to action using @on', () => {
+    class TestComponent extends Component {
+      view = state => state
+
+      @on()
+      f1 = (state, val) => val
+      
       @on('hi2, #hi2')
       f2 = (state, val) => val
     }
 
     const test = new TestComponent().mount();
     const spy = spyOn(test, 'view')
-    test.run('hi', 'x')
+    test.run('f1', 'x')
     expect(spy).toHaveBeenCalledWith('x')
-    app.run('#hi','xx')
+    test.run('f2', 'xx')
     expect(spy).toHaveBeenCalledWith('xx')
+    // local alias
     test.run('hi2', 'xxx')
     expect(spy).toHaveBeenCalledWith('xxx')
+    // global alias
     app.run('#hi2', 'xxxx')
     expect(spy).toHaveBeenCalledWith('xxxx')
-    
-  })
+
+  })  
 
 }) 
