e7612d60714e25a8a513d792138f66aa951182b8
Yiyi Sun <yiyisun@gmail.com>
2018-02-19 23:41:46 -0500

    auto open browser after build

--- cli-templates/webpack.config.js
+++ cli-templates/webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
     ]
   },
   devServer: {
+    open: true
   },
   devtool: 'source-map'
 }
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
     ]
   },
   devServer: {
+    open: true
   },
   devtool: 'source-map'
 }
