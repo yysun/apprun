abbaded9ba29d03890db186ed343f97e6d7de948
Yiyi Sun <yiyisun@gmail.com>
2018-04-21 10:14:52 -0400

    add web-component

---
+++ src/web-component.ts
@@ -0,0 +1,6 @@
+import { Component } from './component';
+
+
+export const toCustomElement = (component: Component): HTMLElement => {
+  return new HTMLElement()
+}
---
+++ tests/custom-element.spec.tsx
@@ -0,0 +1,14 @@
+import app from '../src/app';
+import { createElement, render } from '../src/vdom';
+import { Component } from '../src/component';
+
+import { toCustomElement } from '../src/web-component';
+
+describe('component', () => {
+  it('should be able to export as custom element', () => {
+
+
+
+
+  })
+})
