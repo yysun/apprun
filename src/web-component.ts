bedf7d9810eb11fcbb6dd08c81e5c50dc0eeeaec
Yiyi Sun <yiyisun@gmail.com>
2018-04-23 16:37:14 -0400

    add option to enable shadow DOM

--- src/web-component.ts
+++ src/web-component.ts
@@ -2,14 +2,14 @@ import { Component } from './component';
 declare var customElements;
 
 export const customElement = (componentClass, options?) => class extends HTMLElement {
-   private component;
-   public on;
-   public run;
-  // shadowRoot;
+  private _shadowRoot;
+  private _component;
+  public on;
+  public run;
   constructor() {
     super();
-    // this.shadowRoot = this.attachShadow({ mode: 'open' });
-    // this.shadowRoot.innerHTML = `<div></div>`
+    this._shadowRoot = options && options.shadow ?
+      this.attachShadow({ mode: 'open' }) : this;
     const props = {}
     Array.from(this.attributes).forEach(item => props[item.name] = item.value);
     if (this.children) {
@@ -17,11 +17,11 @@ export const customElement = (componentClass, options?) => class extends HTMLEle
       props['children'].forEach(el => el.parentElement.removeChild(el));
     }
     const opts = { global_event: options && options.global_event };
-    this.component = new componentClass(props).start(this, opts);
-    this.on = this.component.on.bind(this.component);
-    this.run = this.component.run.bind(this.component);
+    this._component = new componentClass(props).start(this._shadowRoot, opts);
+    this.on = this._component.on.bind(this._component);
+    this.run = this._component.run.bind(this._component);
   }
-  get state() { return this.component.state; }
+  get state() { return this._component.state; }
 }
 
 export default (name: string, componentClass, options?) => {
