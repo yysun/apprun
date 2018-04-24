e8677f737d4a5266cea5f493766e196bc0c74674
Yiyi Sun <yiyisun@gmail.com>
2018-04-24 14:41:57 -0400

    add the option to disable auto redner of web component

--- src/web-component.ts
+++ src/web-component.ts
@@ -1,14 +1,15 @@
 import { Component } from './component';
 declare var customElements;
 
-export const customElement = (componentClass, options?) => class extends HTMLElement {
+export const customElement = (componentClass, options={}) => class extends HTMLElement {
   private _shadowRoot;
   private _component;
   public on;
   public run;
   constructor() {
     super();
-    this._shadowRoot = options && options.shadow ?
+    const opts = { global_event: true, render: true, shadow: false, ...options };
+    this._shadowRoot = opts.shadow ?
       this.attachShadow({ mode: 'open' }) : this;
     const props = {}
     Array.from(this.attributes).forEach(item => props[item.name] = item.value);
@@ -16,8 +17,7 @@ export const customElement = (componentClass, options?) => class extends HTMLEle
       props['children'] = Array.from(this.children);
       props['children'].forEach(el => el.parentElement.removeChild(el));
     }
-    const opts = { global_event: options && options.global_event };
-    this._component = new componentClass(props).start(this._shadowRoot, opts);
+    this._component = new componentClass(props).mount(this._shadowRoot, opts);
     this.on = this._component.on.bind(this._component);
     this.run = this._component.run.bind(this._component);
   }
