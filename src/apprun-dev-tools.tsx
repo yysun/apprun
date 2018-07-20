76b6b3fb4b27fa819ca0fd86df3a3c980d5b2745
Yiyi Sun <yiyisun@gmail.com>
2018-07-20 17:49:45 -0400

    update dev-tools

--- src/apprun-dev-tools.tsx
+++ src/apprun-dev-tools.tsx
@@ -1,17 +1,17 @@
 import app from './app';
 import toHTML from './vdom-to-html';
 
-const log = p => console.log(p);
-let debug = false;
+const commands = {};
 
-window['_debug'] = (on) => {
-  on ? !debug && app.on('debug', log) : debug && app.off('debug', log);
-  debug = on;
-}
+commands['help'] = ['', () => {
+  Object.keys(commands).forEach(cmd => cmd === 'help' ?
+    console.log('AppRun Commands:'):
+    console.log(`* ${cmd}: ${commands[cmd][0]}`));
+}];
 
 function newWin(html) {
-  const newWin = window.open('', '_apprun_debug', 'toolbar=0');
-  newWin.document.write(`<html>
+  const win = window.open('', '_apprun_debug', 'toolbar=0');
+  win.document.write(`<html>
   <title>AppRun Analyzer | ${document.location.href}</title>
   <style>
     body { font-family: -apple-system, BlinkMacSystemFont, "Segoe UI" }
@@ -22,7 +22,7 @@ function newWin(html) {
   </script>
   </body>
   </html>`);
-  newWin.document.close();
+  win.document.close();
 }
 
 const Events = ({ events }) => <ul>
@@ -47,7 +47,7 @@ const view = state => <ul>
   </li>)}
 </ul>
 
-window['_components'] = (print) => {
+const _components = (print?) => {
   const o = { components: {} };
   app.run('get-components', o);
   const { components } = o;
@@ -65,3 +65,43 @@ window['_components'] = (print) => {
     });
   }
 }
+
+let debugging = 0;
+app.on('debug', p => {
+  if (debugging & 1 && p.event) console.log(p);
+  if (debugging & 2 && p.vdom) console.log(p);
+});
+
+commands['log'] = ['log [event|view] on|off', (a1?, a2?) => {
+  if (a1 === 'on') {
+    debugging = 3;
+  } else if (a1 === 'off') {
+    debugging = 0;
+  } else if (a1 === 'event') {
+    if (a2 === 'on') {
+      debugging |= 1;
+    } else if (a2 === 'off') {
+      debugging &= ~1;
+    }
+  } else if (a1 === 'view') {
+    if (a2 === 'on') {
+      debugging |= 2;
+    } else if (a2 === 'off') {
+      debugging &= ~2;
+    }
+  }
+  console.log(`log ${a1} ${a2||''}`)
+}];
+
+commands['components'] = ['components log|print', (p) => {
+  _components(p === 'print');
+}]
+
+window['_apprun'] = (strings) => {
+  const [cmd, ...p] = strings[0].split(' ').filter(c => !!c);
+  if (commands[cmd]) commands[cmd][1](...p);
+  // else console.log('Unknown command: ' + cmd);
+  else commands['help'][1]();
+}
+
+console.info('AppRun Commands 0.1: type "_apprun `help`" to list all available commands.');
