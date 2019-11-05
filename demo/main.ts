5d7e37e1136e1bbc1dd93bc8483fc729db0dadc2
Yiyi Sun <yiyisun@gmail.com>
2019-11-05 11:04:09 -0500

    add more examples

--- demo/main.ts
+++ demo/main.ts
@@ -5,19 +5,19 @@ app.on(ROUTER_EVENT, route => {
   for (let i = 0; i < menus.length; ++i) menus[i].classList.remove('active');
   const item = document.querySelector(`[href='${route}']`);
   item && item.parentElement.classList.add('active');
-})
+});
 
 import home from './components/home';
-// import counter from './components/counter';
-// import counters from './components/counters';
-// import dragdrop from './components/dragdrop';
-// import blade from './components/blade';
-// import todo from './components/todo';
+import counter from './components/counter';
+import counters from './components/counters';
+import dragdrop from './components/dragdrop';
+import calculator from './components/calculator';
+import todo from './components/todo';
 import benchmark from './components/benchmark';
-// import hello from './components/hello';
-// import svg from './components/svg';
-// import animation from './components/animation';
+import hello from './components/hello';
+import svg from './components/svg';
+import animation from './components/animation';
 import play from './components/play';
 
 const element = document.getElementById('my-app');
-[home, benchmark, play].forEach(c => c(element));
+[home, hello, counter, counters, todo, calculator, dragdrop, svg, animation, benchmark, play].forEach(c => c(element));
