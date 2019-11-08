a6bd32de9f7af73e07d950d83effc5da413de400
Yiyi Sun <yiyisun@gmail.com>
2019-11-07 19:53:15 -0500

    fix ref test

--- tests/ref.spec.tsx
+++ tests/ref.spec.tsx
@@ -2,11 +2,13 @@ import app from '../src/apprun';
 
 describe('ref', () => {
 
-  it('should set call ref function', () => {
-    const ref = jasmine.createSpy();
+  it('should set call ref function', done => {
+    const ref = e => {
+      expect(e.id).toBe('a');
+      done();
+    }
     const View = () => <div id="a" ref={ref}></div>
     app.render(document.body, <View />);
-    expect(ref).toHaveBeenCalled();
   });
 
 })
