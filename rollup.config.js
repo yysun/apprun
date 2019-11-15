0d9ad8e1c782f62f0b3dc8bc5a826f8ea51c96c5
Yiyi Sun <yiyisun@gmail.com>
2019-11-15 12:02:02 -0500

    add apprun-html.esm.js to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,39 @@
 import filesize from 'rollup-plugin-filesize';
 import { terser } from 'rollup-plugin-terser';
 import sourcemaps from 'rollup-plugin-sourcemaps';
+import resolve from 'rollup-plugin-node-resolve';
 
-export default {
+const plugins = [
+  resolve(),
+  terser({
+    warnings: true,
+    module: true,
+    mangle: {
+      properties: {
+        regex: /^__/,
+      },
+    },
+  }),
+  filesize({
+    // showBrotliSize: true,
+  }),
+  sourcemaps()
+];
+
+export default [{
   input: 'esm/apprun.js',
   output: {
     file: 'dist/apprun.esm.js',
     format: 'esm',
     sourcemap: true
   },
-  plugins: [
-    terser({
-      warnings: true,
-      module: true,
-      mangle: {
-        properties: {
-          regex: /^__/,
-        },
-      },
-    }),
-    filesize({
-      // showBrotliSize: true,
-    }),
-    sourcemaps()
-  ]
-}
+  plugins,
+}, {
+  input: 'esm/apprun-html.js',
+  output: {
+    file: 'dist/apprun-html.esm.js',
+    format: 'esm',
+    sourcemap: true
+    },
+  plugins
+}]
