9b785639cc06660d33b9d25e4bcf2d8efcb40cc7
Yiyi Sun <yiyisun@gmail.com>
2019-11-16 16:54:44 -0500

    :npm run build

--- esm/apprun-html.js
+++ esm/apprun-html.js
@@ -1,8 +1,13 @@
 import app from './apprun';
 export { app, Component, on, update, event, customElement, ROUTER_404_EVENT, ROUTER_EVENT } from './apprun';
-import { createElement, render, Fragment } from './vdom-html';
+import { createElement, render, Fragment, html, svg } from './vdom-lit-html';
+export { html, svg };
 app.createElement = createElement;
 app.render = render;
 app.Fragment = Fragment;
 export default app;
+if (typeof window === 'object') {
+    window['html'] = html;
+    window['svg'] = svg;
+}
 //# sourceMappingURL=apprun-html.js.map
