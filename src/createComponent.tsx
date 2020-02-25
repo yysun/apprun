d80a5ada1ebeff7a4d434184160876c0d70bf957
Yiyi Sun <yiyisun@gmail.com>
2020-02-24 21:01:25 -0500

    fix bug: component not unload

--- src/createComponent.tsx
+++ src/createComponent.tsx
@@ -30,7 +30,7 @@ function render(node, parent, idx) {
     const vdom = component._view(state, props);
     const render = el => {
       component.element = el;
-      component.rendered && component.rendered(state)
+      component.renderState(state, vdom);
     }
     return <section {...props} id={id} ref={e => render(e)}>{vdom}</section>;
   }
