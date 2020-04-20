24e7f25bfd25be389c8fbfac3952f9746879a37d
Yiyi Sun <yiyisun@gmail.com>
2020-04-19 20:07:45 -0400

    demo: redo benchmark

--- demo/components/benchmark.tsx
+++ demo/components/benchmark.tsx
@@ -1,101 +1,60 @@
 import app, { Component } from '../../src/apprun'
-import Store from './store';
+import actions from './store';
+const { run, runLots, add, update, swapRows, clear } = actions;
+
+const state = {
+  data: [],
+  selected: null
+}
+
+type State = typeof state;
+type Events = 'delete'| 'select';
 
 let startTime;
-let lastMeasure;
-
-const startMeasure = function (name) {
+const startMeasure = function () {
   startTime = performance.now();
-  lastMeasure = name;
 }
-const stopMeasure = function () {
-  window.setTimeout(function () {
-    const stop = performance.now();
-    console.log(lastMeasure + " took " + (stop - startTime));
-  }, 0);
-}
-
-const store = new Store();
-
-const update = {
-  '#benchmark': (store) => store,
-
-  run(store) {
-    store.run();
-    return store;
-  },
-
-  add(store) {
-    store.add();
-    return store;
-  },
-
-  remove(store, id) {
-    store.delete(id);
-    document.getElementById(id).remove();
-  },
-
-  select(store, id) {
-    if (store.selected) {
-      document.getElementById(store.selected).className = '';
-    }
-    store.select(id);
-    document.getElementById(id).className = 'danger';
-  },
-
-  updaterow(store) {
-    store.update();
-    return store;
-  },
-
-  runlots(store) {
-    store.runLots();
-    return store;
-  },
-
-  clear(store) {
-    store.clear();
-    document.getElementById("main-table").innerHTML = "";
-    //return store;
-  },
-
-  swaprows(store) {
-    store.swapRows();
-    return store;
+  const stopMeasure = function () {
+    window.setTimeout(function () {
+      const stop = performance.now();
+      console.log(stop - startTime);
+    }, 0);
   }
-}
 
-const view = (model) => {
-  const rows = model.data.map((curr) => {
-    const selected = curr.id === model.selected ? 'danger' : undefined;
-    const id = curr.id;
-    return <tr class={selected} id={id} key={id}>
-    <td class="col-md-1">{id}</td>
-      <td class="col-md-4">
-        <a class="lbl">{curr.label}</a>
-      </td>
-      <td class="col-md-1">
-        <a class="remove">
-          <span class="glyphicon glyphicon-remove remove" aria-hidden="true"></span>
-        </a>
-      </td>
-      <td class="col-md-6"></td>
-    </tr>;
-  });
+const Row = ({ selected, id, label }) => <tr class={selected} id={id} key={id}>
+  <td class="col-md-1">{id}</td>
+  <td class="col-md-4">
+    <a class="lbl">{label}</a>
+  </td>
+  <td class="col-md-1">
+    <a class="remove">
+      <span class="glyphicon glyphicon-remove remove" aria-hidden="true"></span>
+    </a>
+  </td>
+  <td class="col-md-6"></td>
+</tr>;
 
-  return <div id="benchmark" onclick={click}>
+const view = state => {
+  startMeasure();
+  return <div id="benchmark">
     <div>
-      <button type='button' id='run'>Create 1,000 rows</button>
-      <button type='button' id='runlots'>Create 10,000 rows</button>
-      <button type='button' id='add'>Append 1,000 rows</button>
-      <button type='button' id='updaterow'>Update every 10th row</button>
-      <button type='button' id='clear'>Clear</button>
-      <button type='button' id='swaprows'>Swap Rows</button>
+      <button $onclick={run}>Create 1,000 rows</button>
+      <button $onclick={runLots}>Create 10,000 rows</button>
+      <button $onclick={add}>Append 1,000 rows</button>
+      <button $onclick={update}>Update every 10th row</button>
+      <button $onclick={clear}>Clear</button>
+      <button $onclick={swapRows}>Swap Rows</button>
     </div>
     <br />
-    <table class="table table-hover table-striped test-data" id="main-table">
-      <tbody>{rows}</tbody>
+    <table class="table table-hover table-striped test-data" id="main-table" onclick={click}>
+      <tbody>
+        {state.data.map(item => {
+          const selected = item.id == state.selected ? 'danger' : undefined;
+          return <Row {...{ selected, ...item }} />;
+        })}
+      </tbody>
     </table>
+    <span class="preloadicon glyphicon glyphicon-remove hidden" aria-hidden="true"></span>
   </div>
 }
 
@@ -109,28 +68,21 @@ const getId = (elem) => {
   return undefined;
 }
 
-const click = (e) => {
+const click = e => {
   const t = e.target as HTMLElement;
   if (!t) return;
-  if (t.tagName === 'BUTTON' && t.id) {
-    startMeasure(t.id);
-    component.run(t.id);
-    stopMeasure();
-  } else if (t.matches('.remove')) {
-    startMeasure('remove');
-    component.run('remove', getId(t));
-    stopMeasure();
+  if (t.matches('.remove')) {
+    startMeasure();
+    component.run('delete', getId(t));
   } else if (t.matches('.lbl')) {
-    startMeasure('select');
+    startMeasure();
     component.run('select', getId(t));
-    stopMeasure();
   }
 };
 
-const component = new Component(store, view, update);
+const component = new Component<State, Events>(state, view, actions);
 component['-patch-vdom-on'] = true;
-component.unload = () => { store.clear(); console.log('benchmark.unload') };
-component.rendered = () => {
-  store.selected && (document.getElementById(store.selected).className = 'danger');
-}
-export default (element) => component.mount(element);
+component.unload = () => { actions.clear(); console.log('benchmark.unload') };
+component.rendered = () => stopMeasure();
+
+export default (element) => component.mount(element, {route: '#benchmark'});
