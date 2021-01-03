67fa5788beb316f9b1aec00ed2495a2b598018ad
Yiyi Sun <yiyisun@gmail.com>
2021-01-03 13:41:28 -0500

    DevTool: exclude the . event

--- src/apprun-dev-tools.tsx
+++ src/apprun-dev-tools.tsx
@@ -40,7 +40,7 @@ const get_components = () => {
 const viewComponents = state => {
 
   const Events = ({ events }) => <ul>
-    {events && events.map(event => <li>
+    {events && events.filter(event => event.name !== '.').map(event => <li>
       {event.name}
     </li>)}
   </ul>;
@@ -70,7 +70,7 @@ const viewEvents = state => {
 
   const Events = ({ events, global }) => <ul>
     {events && events
-      .filter(event => event.global === global && event.name !== '.')
+      .filter(event => event.global === global && event !== '.')
       .map(({ event, components }) => <li>
         <div>{event}</div>
         <Components components={components} />
