f16a53d8379c707b78341920d13140e490e66782
Yiyi Sun <yiyisun@gmail.com>
2021-02-09 23:22:25 -0500

    CLI: update esbuild build script

--- cli-templates/_build.js
+++ cli-templates/_build.js
@@ -1,30 +1,27 @@
 #!/usr/bin/env node
 const builder = require('esbuild');
-const path = require('path');
 const server = require('apprun-dev-server');
 
-const app = {
-  build: () => builder.build({
-    entryPoints: ['src/main.tsx'],
-    outfile: 'dist/main.js',
-    bundle: true,
-    minify: true,
-    sourcemap: true
-  }),
+const build = (watch = false) => builder.build({
+  entryPoints: ['src/main.tsx'],
+  outfile: 'dist/main.js',
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  watch
+});
 
-  start: () => {
-    server.start({
-      host: 'localhost',
-      port: process.env.PORT || 8080,
-      watch: 'src'
-    });
+const start = () => {
+  server.start({
+    host: 'localhost',
+    port: process.env.PORT || 8080,
+    watch: 'src'
+  });
+}
 
-    server.watcher.on('change', (changePath) => {
-      const ext = path.extname(changePath).toLocaleLowerCase();
-      /\.(t|j)sx?/.test(ext) && app.build();
-    });
-  }
-};
-
-app.build();
-process.argv[2] === 'start' && app.start();
+if (!process.argv[2]) {
+  build();
+} else {
+  build(true);
+  start();
+}
