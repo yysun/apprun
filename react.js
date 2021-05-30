2f093f089096bd2971acf8bd5009537ba6b2ec43
Yiyi Sun <yiyisun@gmail.com>
2021-05-30 11:00:20 -0400

    add react.js from coverting components to React

---
+++ react.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const toReact = (componentClass) => {
+  const component = new componentClass().mount();
+  const { state, view } = component;
+  return () => {
+    const [_state, setState] = useState(state);
+    component.view = setState;
+    return <>{view(_state)}</>;
+  }
+}
+
+export default toReact;
