a29b2cea84ea755f1103ee2e381ffaa354f0e04a
Yiyi Sun <yiyisun@gmail.com>
2024-08-12 15:03:53 -0400

    add function use_render and use_react

--- src/apprun.ts
+++ src/apprun.ts
@@ -70,4 +70,15 @@ if (typeof window === 'object') {
   window['safeHTML'] = safeHTML;
 }
 
+app['use_render'] = (render, mode = 0) =>
+  mode === 0 ?
+    app.render = (el, vdom) => render(vdom, el) : // react style
+    app.render = (el, vdom) => render(el, vdom);  // apprun style
 
+app['use_react'] = createRoot => {
+  app.render = (el, vdom) => {
+    if (!el || !vdom) return;
+    if (!el._root) el._root = createRoot(el);
+    el._root.render(vdom);
+  }
+}
