1ef4b946842169e1a0340899e884ecf9eb2b3406
Yiyi Sun <yiyisun@gmail.com>
2024-08-29 13:42:26 -0400

    add user_react to react.js file

--- react.js
+++ react.js
@@ -1,5 +1,20 @@
+import app from 'apprun';
+import React from 'react';
+import ReactDOM from 'react-dom/client'
 import { useState } from 'react';
 
+export const user_react = () => {
+  app.h = app.createElement = React.createElement;
+  app.Fragment = React.Fragment;
+  app.render = (el, vdom) => ReactDOM.render(vdom, el);
+  if (React.version && React.version.startsWith('18')) {
+    app.render = (el, vdom) => {
+      if (!el || !vdom) return;
+      if (!el._root) el._root = ReactDOM.createRoot(el);
+      el._root.render(vdom);
+    }
+  }
+}
 const toReact = (componentClass) => {
   const component = new componentClass().mount();
   const { state, view } = component;
