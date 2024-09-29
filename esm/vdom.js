49f6f5928575aa34151dba6dbac4cd009ccf5587
Yiyi Sun <yiyisun@gmail.com>
2024-09-29 13:08:40 -0400

    :compile

--- esm/vdom.js
+++ esm/vdom.js
@@ -1,3 +1,4 @@
 import { createElement, updateElement, Fragment, safeHTML } from './vdom-my';
 export { createElement, Fragment, updateElement as render, safeHTML };
+export { createElement as jsx, createElement as jsxs };
 //# sourceMappingURL=vdom.js.map
